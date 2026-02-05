d0aee7126c01f760151949a055510b7598feaea3
bleubirb <ispott@cspott.com>
2026-02-04 18:00:21 -0800

    pushing start to http server

---
+++ http_server/httpserver.c
@@ -0,0 +1,12 @@
+/* homemade http server for bread hosting
+ * hopefully it'll work who knows
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <regex.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
